a3a71a3af97953244f4bf49b742339158cf8f3e1
Avila <christopheravila49@gmail.com>
2021-08-03 19:57:01 -0600

    Area Rectangulo

--- src/poofiguras/Figuras.java
+++ src/poofiguras/Figuras.java
@@ -21,13 +21,21 @@ public class Figuras {
         JOptionPane.showMessageDialog(null, "El area del triangulo es :"+area);
         
     }
-       public void circulo(){
+     public void circulo(){
      double circunferencia,radio,area;
      double pi=3.14;
-        circunferencia = Integer.parseInt(JOptionPane.showInputDialog("Digite la circunferencia del circulo"));
+        circunferencia = Double.parseDouble(JOptionPane.showInputDialog("Digite la circunferencia del circulo"));
         radio=(circunferencia*pi)/2;
         area=(pi*Math.pow(radio,radio))/2;
         JOptionPane.showMessageDialog(null, "El area del circulo  es :"+area);
+        
+    }
+     public void rectangulo(){
+     double base,altura,area;
+        base = Double.parseDouble(JOptionPane.showInputDialog("Digite la base del rectangulo"));
+        altura=Double.parseDouble(JOptionPane.showInputDialog("Digite la altura del rectangulo"));
+        area=(base*altura);
+        JOptionPane.showMessageDialog(null, "El area del rectangulo es :"+area);
         
     }
 
