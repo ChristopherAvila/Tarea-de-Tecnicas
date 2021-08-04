c3d84a8c19bbfb9f01630c46e4a6dd2c6137830c
Avila <christopheravila49@gmail.com>
2021-08-03 19:14:41 -0600

    Tarea de Tecnicas de Progrmacion

---
+++ src/poofiguras/principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poofiguras;
+
+/**
+ *
+ * @author PC
+ */
+public class principal {
+   public static void main(String[] args){
+   
+   Figuras datos =new Figuras();
+   datos.cuadrado();
+   datos.triangulo();
+   } 
+          
+    
+}
